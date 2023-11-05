f094ac5966f01bd5cfffa94e724c28c82f14fd8f
Genius-er <771282518@qq.com>
2023-11-05 11:06:48 +0800

    python环境检测和安装python环境，并安装工具依赖包，使用python3.11运行

--- main.bat
+++ main.bat
@@ -1,16 +1,35 @@
-REM 检查是否已经安装了Python3
-python --version >nul 2>nul
-IF %ERRORLEVEL% EQU 0 (
-    REM 如果已经安装了Python3，则直接运行脚本
-    py -3 ./src/main.py
-) ELSE (
-    REM 如果没有安装Python3，则下载最新版的Python3安装包
-    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe', 'python-3.9.7-amd64.exe')"
-    REM 安装Python3
-    python-3.9.7-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-    REM 下载openpyxl和pillow
-    py -3 -m pip install openpyxl pillow
-    REM 运行脚本
-    py -3 ./src/main.py
+@echo off
+chcp 65001
+
+set "pythonVersion=3.11"
+
+REM 检查注册表中是否存在 Python 3.11 版本的安装路径
+for /f "tokens=2*" %%A in ('reg query "HKLM\Software\Python\PythonCore\3.11\InstallPath" /ve 2^>nul') do (
+    set "pythonPath=%%B"
 )
-pause
+for /f "tokens=2*" %%A in ('reg query "HKCU\Software\Python\PythonCore\3.11\InstallPath" /ve 2^>nul') do (
+    set "pythonPath=%%B"
+)
+
+set "downloadVersion=3.11.6"
+if not defined pythonPath (
+    echo Python %pythonVersion% 未在计算机上找到。
+    rem 下载 Python 安装包
+    bitsadmin /transfer PythonInstaller https://www.python.org/ftp/python/%downloadVersion%/python-%downloadVersion%-amd64.exe %temp%\python-%downloadVersion%-amd64.exe
+    
+    rem 静默安装 Python
+    %temp%\python-%downloadVersion%-amd64.exe /quiet InstallAllUsers=1 PrependPath=0
+    
+    rem 删除安装包
+    del %temp%\python-%downloadVersion%-amd64.exe
+    echo 安装python%downloadVersion%完成
+) else (
+    echo Python %pythonVesion% 已安装在 %pythonPath%
+)
+
+rem 安装依赖
+py -3.11 -m pip install -r requirements.txt
+
+
+rem 运行正式工具脚本
+py -3.11 .\src\main.py
